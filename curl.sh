1974d783428417b7f664cc2c0e6bb79d44f485f1
sirius <sirius.ustc@gmail.com>
2018-06-02 05:00:03 +0800

    2018-06-02

--- curl.sh
+++ curl.sh
@@ -4,7 +4,8 @@
 [[ -z $1 ]] && workdir='/root/git/bing-wallpaper/assets/' || workdir=$1
 site=cn.bing.com
 cd $workdir
-text=`wget -O- $site`
+text=`wget -nv -O- $site`
+#text=`curl -L $site`
 if [[ -z $text ]]; then
 	echo "connecting to $site failed!" | mail -s "bing-wallpaper" root
 fi
@@ -15,7 +16,7 @@ if [[ -e $filename ]]; then
 	echo "file exist!" | mail -s "bing-wallpaper" root
 	echo "file exist!"
 else
-	wget -O $workdir$filename https://$site/$url
+	wget -nv -O $workdir$filename https://$site/$url
 	git add $filename
 	git commit -a -m `date -I`
 	git push origin master
