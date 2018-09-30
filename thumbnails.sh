b11b6d47bbc3039de6b00311d27cc21d4ed5382e
sirius <sirius.ustc@gmail.com>
2018-09-30 11:09:55 +0800

    add the thumbnails auto-generator script

---
+++ thumbnails.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source=${HOME}/bing-wallpaper/assets/
+target=${HOME}/bw-thumbnails/
+date=`date -I`
+mkdir -p ${target}${date}
+for i in $(diff <(ls $source) <(ls $target) | grep '<' | grep -v '.png' | cut -d" " -f 2)
+do echo "convert ${source}$(basename $i) -resize 10% ${target}${date}/$(basename $i)"
+convert ${source}$(basename $i) -resize 10% ${target}${date}/$(basename $i)
+done
