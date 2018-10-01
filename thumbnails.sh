cd3202e7928f3855dd148f34ae7fbefb425bf4b6
sirius <sirius.ustc@gmail.com>
2018-10-01 20:14:33 +0800

    make link after resize the file, and no need to move photos in temporary folder into main folder

--- thumbnails.sh
+++ thumbnails.sh
@@ -7,4 +7,5 @@ mkdir -p ${target}${date}
 for i in $(diff <(ls $source) <(ls $target) | grep '<' | grep -v '.png' | cut -d" " -f 2)
 do echo "convert ${source}$(basename $i) -resize 10% ${target}${date}/$(basename $i)"
 convert ${source}$(basename $i) -resize 10% ${target}${date}/$(basename $i)
+ln ${target}${date}/$(basename $i) ${target}/$(basename $i)
 done
